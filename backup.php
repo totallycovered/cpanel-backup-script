48e6afd4ec79f866d0e674bcb0d9e57a73fbf44a
totallycovered <jason@totallycovered.net>
2017-07-14 16:25:30 -0400

    Create backup.php

---
+++ backup.php
@@ -0,0 +1,23 @@
+<?php
+  $home = $_SERVER['HOME'];
+	$client = $home . "/scripts/clients/";
+  	
+	if ($handle = opendir($client)) {
+		while (false !== ($file = readdir($handle))) {
+			if ('.' === $file) continue;
+			if ('..' === $file) continue;
+	
+			// Run the backup
+      include $home . '/scripts/clients/' . $file;
+			include $home . '/scripts/include/ftp.php';
+			include $home . '/scripts/include/notification.php';
+			include $home . '/scripts/include/backup.php';
+			sleep(15);
+					
+		}
+		closedir($handle);
+	}
+	
+    mail("email@example.com","cPanel Backup Script","Completed");
+
+?>
