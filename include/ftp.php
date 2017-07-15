37397c149d81810fc0d6b900e8df4276dfdc51b2
totallycovered <jason@totallycovered.net>
2017-07-15 18:43:55 -0400

    Create ftp.php

---
+++ include/ftp.php
@@ -0,0 +1,7 @@
+<?php
+	$ftpuser = "username"; // Username for FTP account
+	$ftppass = "password"; // Password for FTP account
+	$ftphost = "example.com"; // Full hostname or IP address for FTP host
+	$ftpmode = "passiveftp"; // FTP mode ("ftp" for active, "passiveftp" for passive)
+	$ftpport = "21"; // FTP port, usually 21
+?>
