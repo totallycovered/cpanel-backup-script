50a206bdc27e9d0a7a43b4eca90ab7ba451e2b12
totallycovered <jason@totallycovered.net>
2017-07-15 18:49:58 -0400

    Create example.com.php

---
+++ clients/example.com.php
@@ -0,0 +1,8 @@
+<?php
+	// Info required for cPanel access
+	$cpuser = "example"; // Username used to login to CPanel
+	$cppass = "password"; // Password used to login to CPanel
+	$domain = "example.com"; // Domain name where CPanel is run
+	$secure = 0; // Set to 1 for SSL (requires SSL support), otherwise will use standard HTTP
+	$skin = "paper_lantern"; // Set to cPanel skin you use (script won't work if it doesn't match)
+?>
