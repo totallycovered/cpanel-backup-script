3a2f1a35bdd56c7a34cc0977de600559c7ddd40e
totallycovered <jason@totallycovered.net>
2017-07-16 01:53:12 -0400

    Update ftpcheck.php
    - created $conn_id, $login_result, $create
    - input $debug from config.

--- include/ftpcheck.php
+++ include/ftpcheck.php
@@ -1,22 +1,32 @@
 <?php
 
+// This script checks the FTP scredentials and created backup folder
+
 // Usable $trings
 // $ftpuser $ftppass $ftphost $ftpmode $ftpport $ftpdir $notifyemail
 
 // Set up FTP connection and test.
 $conn_id = ftp_connect($ftphost, $ftpport) or die("Couldn't connect to FTP server.");
 
-// TEst FTP login
+// FTP login results
 $login_result = ftp_login($conn_id, $ftpuser, $ftppass);
 
 // try to create the directory $ftpdir
 if (ftp_mkdir($conn_id, $ftpdir)) {
- echo "successfully created $dir\n";
+ $create = "Sucessfully!\n";
 } else {
- echo "There was a problem while creating $dir\n";
+ $create = "There was a problem while creating the backup folder\n";
 }
 
 // close the connection
 ftp_close($conn_id);
+
+// Debug
+if ($debug) {
+ echo "Login Result: $login_result\n";
+ sleep(10);
+ echo "Create Backup Folder: $create\n";
+ sleep(10);
+}
 
 ?>
