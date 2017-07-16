998f4061e06e7f361dd914a844797eb3663e8ab5
totallycovered <jason@totallycovered.net>
2017-07-16 02:01:59 -0400

    Update and rename variables.conf to config.php
    - added $debug

--- config/config.php
+++ config/config.php
@@ -6,10 +6,12 @@ $ftppass = ""; // Password for FTP account
 $ftphost = "t"; // Full hostname or IP address for FTP host
 $ftpmode = "passiveftp"; // FTP mode ("ftp" for active, "passiveftp" for passive)
 $ftpport = "21"; // FTP port, usually 21
-$ftpdir = "backups/" . $domain;
-
+$ftpdir = "backups/"; // It will be created with domain subfolder
 
 // Notification information
 $notifyemail = ""; // Email address to send results
+
+// Debug mode
+$debug = 0;
 
 ?>
