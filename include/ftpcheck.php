f8feaa6b6333441f0e0f1f8e4c4e86705b87a211
totallycovered <jason@totallycovered.net>
2017-07-16 02:21:31 -0400

    Update ftpcheck.php
    added some debug stuff

--- include/ftpcheck.php
+++ include/ftpcheck.php
@@ -2,20 +2,38 @@
 
 // This script checks the FTP scredentials and created backup folder
 
-// Usable $trings
+//  Usable $trings
 // $ftpuser $ftppass $ftphost $ftpmode $ftpport $ftpdir $notifyemail
+
+// Debug
+if ($debug) {
+ echo "Usable varibles are $ftpuser $ftppass $ftphost $ftpmode $ftpport $ftpdir $notifyemail\n"
+ sleep(10);
+}
 
 // Set up FTP connection and test.
 $conn_id = ftp_connect($ftphost, $ftpport) or die("Couldn't connect to FTP server.");
 
 // FTP login results
 $login_result = ftp_login($conn_id, $ftpuser, $ftppass);
+// Debug
+if ($debug) {
+ echo "Login result was $login_result\n"
+ sleep(10);
+}
 
 // try to create the directory $ftpdir
-if (ftp_mkdir($conn_id, $ftpdir)) {
+$dir = $ftpdir . $domain;
+if (ftp_mkdir($conn_id, $dir)) {
  $create = "Sucessfully!\n";
 } else {
  $create = "There was a problem while creating the backup folder\n";
+ die("There was a problem while creating the backup folder.")
+}
+// Debug
+if ($debug) {
+ echo "Create Backup Folder: $create\n";
+ sleep(10);
 }
 
 // close the connection
@@ -23,10 +41,8 @@ ftp_close($conn_id);
 
 // Debug
 if ($debug) {
- echo "Login Result: $login_result\n";
- sleep(10);
- echo "Create Backup Folder: $create\n";
- sleep(10);
+ echo "ftpcheck.php has been ran.\n";
+ sleep(10_;
 }
 
 ?>
